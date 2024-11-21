aee25359c949fefda97ec1d316654f94a4d4067a
hector <hector@cs470.local.com>
2024-11-20 17:36:28 -0800

    idk

--- Assets/Scripts/Interactables/Components/Inspectable.cs
+++ Assets/Scripts/Interactables/Components/Inspectable.cs
@@ -1,18 +1,22 @@
 using TriInspector;
 using UnityEngine;
 using HUD;
+using Interactables;
 
 namespace Interactables.Components
 {
     [HideMonoScript]
     public class Inspectable : MonoBehaviour
     {
+        private Interactives interactable;
         // When the mouse is over the inspectable entity
         public void OnMouseOver()
         {
             if (Input.GetMouseButtonDown(1))
             {
-                // Inspect Code Here
+                Debug.Log("M2 input!");
+                interactable.Hud.invertHudStatus();
+                interactable.Hud.changeText("some text");
             }
         }
 
